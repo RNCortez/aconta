728a25703c776d54ed699d48a09da11059fc95c0
Rafael Cortez <rafael.cortez@hotmail.com>
2019-03-06 13:01:49 -0300

    Projeto criado

---
+++ src/test/java/com/program/aconta/AcontaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.program.aconta;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AcontaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
