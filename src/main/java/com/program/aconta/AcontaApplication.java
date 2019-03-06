8707e3add2c96e2d1ddb46a7e8e5ca61a04e38dc
Rafael Cortez <rafael.cortez@hotmail.com>
2019-03-06 14:53:12 -0300

    add classe de dominio Categoria, repository e service para categoria, e add o banco h2 para testes e fazendo a instancia para testes.

--- src/main/java/com/program/aconta/AcontaApplication.java
+++ src/main/java/com/program/aconta/AcontaApplication.java
@@ -1,14 +1,36 @@
 package com.program.aconta;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AcontaApplication {
+import com.program.aconta.domain.Categoria;
+import com.program.aconta.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class AcontaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AcontaApplication.class, args);
 	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Bebidas");
+		Categoria cat2 = new Categoria(null,"Lanches");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		
+	}
+	
+	
 
 }
 
